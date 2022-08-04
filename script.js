a022b96429ca511b60cacf5e68d755aabb83ae1d
tauri_s <tauristclaire@gmail.com>
2022-08-03 17:13:20 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var programmingLanguages = [
+  "Scala",
+  "Java",
+  "Go",
+  "Swift",
+  "Cobol",
+  "JavaScript",
+  "Python",
+  "Ruby",
+  "Elm",
+  "Rust",
+  "C#"
+];
+
+console.log(programmingLanguages);
